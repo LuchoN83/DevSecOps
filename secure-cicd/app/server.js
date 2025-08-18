0960bf42c5eed02e43b485ed69980c0f96d60e60
Luis Navarrete <luis.navarrete.aranda@hotmail.com>
2025-08-17 22:40:11 -0500

    Creacion de CICD Secure

---
+++ secure-cicd/app/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (_, res) => res.send("Hello from Secure App!"));
+app.get("/health", (_, res) => res.status(200).send("ok"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
